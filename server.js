09b175260b33a86c7d2987d1fcf457f2966be372
Travis Vadnais <travisvadnais@gmail.com>
2018-05-31 21:35:00 -0400

    resolve conflict in server.js

--- server.js
+++ server.js
@@ -3,27 +3,38 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
-var routes = require('./controllers/routes.js');
-var PORT = process.env.PORT || 3000;
 var app = express();
+var PORT = process.env.PORT || 3000;
 var automation = require ("./controllers/automation.js")
 
+var db = require("./models");
 
-app.use(express.static('public'));
+//================Boilerplate Express stuff==========================//
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
 
+// Static directory
+app.use(express.static("public"));
+//==================================================================//
+
+
+//Handlebars stuff (if we use it)
+//===========================================================
 app.use(methodOverride("_method"));
-app.engine('handlebars',exphbs({
-    defaultLayout: "main"
-}));
-
+app.engine('handlebars',exphbs({defaultLayout: "main"}));
 app.set('view engine', 'handlebars');
+//===========================================================
 
-app.use('/', routes);
+//Require Routes
+require("./routes/users-routes.js")(app);
+require("./routes/wagers-routes")(app);
 
-app.listen(PORT);
-console.log(`Listening on ${PORT}`);
-
+//Sync Sequelize models and start express
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+});
