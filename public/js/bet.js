effce6295c10aa25142259490807c10790fcf55c
Pacho Salazar <jfsh87@hotmail.com>
2018-06-06 21:17:13 -0400

    newbet

---
+++ public/js/bet.js
@@ -0,0 +1,27 @@
+
+function createbet(){
+   
+    var drop = $("<select>")
+    
+
+    $.get("/names", userData.nickname)
+    .then(function(userList) {
+
+        for(var i; i < userList.length;i ++){
+        var opt = $("<option>")
+        opt.attr("value", userList[i].id)
+        opt.text(userList[i].nickname)
+        drop.append(opt)        
+       }
+})
+
+
+}
+
+$("#nickname").on('click',function(){
+    createbet()
+})
+// $("#newbet").on('click',function(){
+//     $('.modal').modal();
+//     $('#modalbet').modal();
+// })
